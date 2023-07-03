d446a22a2ad439210f8f8c11188a431a4c9c2a5e
andrey <neform@gmail.com>
2023-07-03 23:04:42 +0200

    feat: added helmet, cookies parser

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,8 @@
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import * as cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 import { NODE_ENV } from "../config/app";
 import { AppModule } from "./app.module";
@@ -10,6 +12,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors();
+  app.use(helmet());
+  app.use(cookieParser());
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>("PORT");
