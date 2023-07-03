b671ad71e7f5dece7711e5df79e4c7e8da47df9e
andrey <neform@gmail.com>
2023-07-03 23:02:57 +0200

    feat: added swagger

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,7 +2,9 @@ import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
 
+import { NODE_ENV } from "../config/app";
 import { AppModule } from "./app.module";
+import { setupSwagger } from "./utils/swagger/setup";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +13,10 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>("PORT");
+
+  if (configService.get<string>("NODE_ENV") === NODE_ENV.DEVELOPMENT) {
+    setupSwagger(app);
+  }
 
   await app.listen(port as number);
 }
